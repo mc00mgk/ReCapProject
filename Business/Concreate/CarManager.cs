b0cbeeaf5298af52f2f3f734651fc9068857c6fb
mc00m <mc00m@MSI>
2022-09-27 13:54:29 +0300

    Add project files.

---
+++ Business/Concreate/CarManager.cs
@@ -0,0 +1,27 @@
+﻿using Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concreate
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+
+        {
+
+            return _carDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Abstract/ICarDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface ICarDal
+    {
+        List<Araba> GetAll();
+        void Ekle(Araba arabası);
+        void Güncelleme(Araba arabası);
+        void Sil(Araba arabası);
+        List<Araba> GetAllById(int carId);
+
+    }
+}
